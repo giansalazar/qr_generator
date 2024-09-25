c770dd835c5288c497271573a46feaf18d9b12d6
Giancarlo Salazar <giancarlosalazar97@gmail.com>
2024-09-25 11:17:12 -0600

    cambios_gh

--- qr_generator/vite.config.js
+++ qr_generator/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/qr_generator/', // Reemplaza <nombre-del-repositorio> con el nombre de tu repositorio
+});
